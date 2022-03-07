e16abb1e34dc4087cac76f083e79396d753380aa
Black-Mandarin <80224569+Black-Mandarin@users.noreply.github.com>
2022-03-07 21:27:07 +1100

    Updating Login and Login Out page

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,11 @@
 const logout = async () => {
-  const response = await fetch("/api/staff/logout", {
+  const response = await fetch("/api/staffs/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace("/login");
   } else {
     alert(response.statusText);
   }
