7e846486e5001ae4b26d556fb19b2eab73a450d6
Ayako Woollan <ayako.woollan@gmail.com>
2022-03-07 22:01:49 +1100

    running the app

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+require('dotenv').config();
+
+
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
+const hbs = exphbs.create({});
 
 const sess = {
     secret: process.env.DB_SECRET,
