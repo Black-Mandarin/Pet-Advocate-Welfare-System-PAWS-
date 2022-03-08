8129f30aa759ca9ca435b3036400e83fd0e8adf0
Sean <seanms418@gmail.com>
2022-03-08 19:53:00 +1100

    add the helper function format_date

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 require('dotenv').config();
 
-
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,9 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
-
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: process.env.DB_SECRET,
