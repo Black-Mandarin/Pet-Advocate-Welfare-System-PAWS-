37913ab0b3380d708170da85d22b5dbc5a371b42
Sean <seanms418@gmail.com>
2022-03-08 19:17:23 +1100

    added the booking routes and changed the app.use variable name for continuity

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-// const bookingRoutes = require('./bookingRoute');
+const bookingRoutes = require('./bookingRoute');
 const staffRoutes = require('./staffRoute');
 
-router.use('/staff', staffRoutes);
-// router.use('/booking', bookingRoutes);
+router.use('/bookings', bookingRoutes);
+router.use('/staffs', staffRoutes);
 
 
 module.exports = router;
