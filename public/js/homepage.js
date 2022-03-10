c8b9aebcd883fee1fc3ba30613413ce9c099dbc5
Ayako Woollan <ayako.woollan@gmail.com>
2022-03-10 22:06:10 +1100

    moving deletebtn function to homepage.js

---
+++ public/js/homepage.js
@@ -0,0 +1,20 @@
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/bookings/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to delete booking');
+        }
+    }
+};
+
+document
+    .querySelector('#deleteBtn')
+    .addEventListener('click', delButtonHandler);
