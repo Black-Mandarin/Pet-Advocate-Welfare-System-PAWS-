094c9e674fbaeb138ca0eb698f52013337f387b6
Sean <seanms418@gmail.com>
2022-03-13 14:31:02 +1100

    changed file names tokebab style

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const bookingRoutes = require('./bookingRoute');
-const staffRoutes = require('./staffRoute');
+const bookingRoutes = require('./booking-routes');
+const staffRoutes = require('./staff-routes');
 
 router.use('/bookings', bookingRoutes);
 router.use('/staffs', staffRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-const bookingRoutes = require('./bookingRoutes');
-const personalRoutes = require('./personalRoutes');
+const homeRoutes = require('./home-routes');
+const bookingRoutes = require('./booking-routes');
+const personalRoutes = require('./personal-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
