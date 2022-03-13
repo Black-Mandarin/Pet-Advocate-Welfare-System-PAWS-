8a048b9cd7c2244218ec6f876068509a5c4fc933
Sean <seanms418@gmail.com>
2022-03-13 19:03:59 +1100

    added the staffs name, email and id to the personal page by adding helper functions

--- utils/helper.js
+++ utils/helper.js
@@ -5,5 +5,23 @@ module.exports = {
     },
     reverse_date: (date) => {
         return date.toLocaleDateString().split("/").reverse().join("-");
+    },
+    get_name: (bookings) => {
+        const res = [];
+        for(var i in bookings)
+                res.push(bookings[i]);
+        return res[0].staff.name;
+    },
+    get_id: (bookings) => {
+        const res = [];
+        for(var i in bookings)
+                res.push(bookings[i]);
+        return res[0].staff.id;
+    },
+    get_email: (bookings) => {
+        const res = [];
+        for(var i in bookings)
+                res.push(bookings[i]);
+        return res[0].staff.email;
     }
 };
