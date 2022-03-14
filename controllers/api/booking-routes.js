ee30abaf0bca0ce823c56266ec992ce8c230d1ed
Ayako Woollan <ayako.woollan@gmail.com>
2022-03-14 11:26:11 +1100

    sending email when  updating, bcc to PAWS email

--- controllers/api/booking-routes.js
+++ controllers/api/booking-routes.js
@@ -34,7 +34,7 @@ router.post("/", withAuth, async (req, res) => {
         console.log(req.session);
 
         const output = `
-        <p>You are assigned as a care staff for the following booking:</p>
+        <p>The following booking has been created:</p>
         <p>Pet Name: ${req.body.pet_name}</p>
         <p>Type: ${req.body.pet_type} </p>
         <p> Breed:${req.body.pet_breed}</p>
@@ -65,7 +65,8 @@ router.post("/", withAuth, async (req, res) => {
         let mailOptions = {
             from: '"Pet Advocate Welfare System" <petadvocatewelfaresystem@gmail.com>', // sender address
             to: req.session.email, // list of receivers
-            subject: 'You are assigned as a Care Staff', // Subject line
+            bcc: '"Pet Advocate Welfare System" <petadvocatewelfaresystem@gmail.com>',
+            subject: 'New Booking has been created.', // Subject line
             text: '', // plain text body
             html: output // html body
         };
@@ -112,6 +113,53 @@ router.put("/:id", withAuth, async (req, res) => {
             res.status(404).json({ message: 'No booking found with this id!' });
             return;
         }
+
+
+        const output = `
+        <p>The following booking has been updated:</p>
+        <p>Pet Name: ${req.body.pet_name}</p>
+        <p>Type: ${req.body.pet_type} </p>
+        <p> Breed:${req.body.pet_breed}</p>
+        <p>Note: ${req.body.pet_notes}</p>
+        <p>Type: ${req.body.pet_type}</p>
+        <p>Breed: ${req.body.pet_breed}</p>
+        <p>Owner: ${req.body.owner_name}</p>
+        <p>Drop off: ${req.body.date_dropoff}</p>
+        <p> Pick up: ${req.body.date_pickup}</p>
+        <p>Fee: ${req.body.fee}</p>
+        Kind regards,
+        PAWS Team`;
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.DB_EMAIL, // generated ethereal user
+                pass: process.env.DB_EMAIL_PASSWORD  // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Pet Advocate Welfare System" <petadvocatewelfaresystem@gmail.com>', // sender address
+            to: req.session.email, // list of receivers
+            bcc: '"Pet Advocate Welfare System" <petadvocatewelfaresystem@gmail.com>',
+            subject: 'Booking has been updated.', // Subject line
+            text: '', // plain text body
+            html: output // html body
+        };
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // res.render('contact', { msg: 'Email has been sent' });
+        });
 
         res.status(200).json({ bookingData, petData });
     } catch (err) {
